479e8c7ecb11ff01d0a70604a9d9f573d2fb3bb5
Markus Nissinen <markus.nissinen42@gmail.com>
2017-11-11 15:38:24 +0000

    Create puppetslaves.sh

---
+++ puppetslaves.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+
+sudo apt-get install -y puppet git
+
+git clone https://github.com/marrkus/Puppetslaves.git
+
+sudo cp -r puppetslaves /etc/puppet/modules/
+
+cd /etc/puppet/
+
+sudo puppet apply --modulepath modules/ -e 'class {"puppetslaves":}'
+
+sudo puppet agent --enable
+
+sudo service puppet restart
+
+echo " "
+echo "Slaving complete!"
+echo " "
